7889fb4adf5e7a7465d1cbdf67d1f236cf514bb8
KRDodge <>
2025-01-24 19:55:32 +0900

    cmake 사용하기

---
+++ DataStruct/DataStruct.cpp
@@ -0,0 +1,29 @@
+#include "DataStruct.h"
+#include "memory.h"
+#include "include/chapter1/memoryTest.h"
+#include "include/chapter1/page20test.h"
+#include "include/chapter1/page27test.h"
+#include "include/chapter1/stl.h"
+
+using namespace std;
+
+using namespace Chapter1;
+
+int main()
+{
+	//Chapter1::NewandDelete();
+	//Chapter1::GetDivZeroInput();
+	//Chapter1::AddArray();
+	//Chapter1::BinarySearch();
+	//Chapter1::Permutations();
+	//Chapter1::AllBool();
+	//Chapter1::Fibonacci();
+
+	//Chapter1::Product();
+	//Chapter1::Copy();
+	//Chapter1::NPermutations();
+	//MagicSquare();
+	SeqSearchTime();
+
+	return 0;
+}
---
+++ DataStruct/src/chapter1/memoryTest.cpp
@@ -0,0 +1,23 @@
+#include "chapter1/memoryTest.h"
+
+using namespace std;
+
+namespace Chapter1 {
+
+	Memory::Memory()
+	{
+	}
+
+	Memory::~Memory()
+	{
+	}
+
+	void NewandDelete()
+	{
+		Memory* m = new Memory();
+		cout << "made class m" << endl;
+
+		delete m;
+		cout << "deleted class m" << endl;
+	}
+}
