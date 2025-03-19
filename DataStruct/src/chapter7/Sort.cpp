74717dfd020b450f9c5bfa3478ff8e95b6cbad6a
KRDodge <>
2025-03-19 22:31:58 +0900

    insert sort 내 방식대로 수정

--- DataStruct/src/chapter7/Sort.cpp
+++ DataStruct/src/chapter7/Sort.cpp
@@ -17,8 +17,9 @@ namespace Chapter7
 	template<class T>
 	void Sort<T>::Insert(const T& e, T* a, int i)
 	{
-		a[0] = e;
-		while (e < a[i])
+		//교재에서는 a[0]를 루프 검사를 더 쉽게 하기 위해 더미 스페이스로 사용하고 있던데
+		//해당 알고리즘 내에서만 더 효율적이지 외부에서 접근했을 때 0인덱스를 못쓰는 문제 발생 (외부에서 0가 더미인지를 모름)
+		while (i >= 0 && a[i] > e) 
 		{
 			a[i + 1] = a[i];
 			i--;
@@ -29,10 +30,10 @@ namespace Chapter7
 	template<class T>
 	void Sort<T>::InsertionSort(T* a, const int n)
 	{
-		for (int j = 2; j <= n; ++j)
+		for (int i = 1; i < n; ++i) 
 		{
-			T temp = a[j];
-			Insert(temp, a, j - 1);
+			T key = a[i];
+			Insert(key, a, i - 1);
 		}
 	}
 }
