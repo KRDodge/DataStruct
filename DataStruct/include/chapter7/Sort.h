3661e1231d6530a91cb398d36a34f097409ef26c
KRDodge <>
2025-03-22 03:53:59 +0900

    3/22 힙정렬

--- DataStruct/include/chapter7/Sort.h
+++ DataStruct/include/chapter7/Sort.h
@@ -30,6 +30,9 @@ namespace Chapter7
 		int rMergeSort(T* a, int* link, const int left, const int right);
 		int ListMerge(T* a, int* link, const int start1, const int start2);
 
+		void HeapSort(T* a, const int n);
+		void Adjust(T* a, const int root, const int n);
+
 	private:
 		list<T>* d; //이제 list 쓰자 vector는 위치 교환 및 삽입 연산 여러번 일어나서 비효율적
 	};
--- DataStruct/src/chapter7/Sort.cpp
+++ DataStruct/src/chapter7/Sort.cpp
@@ -168,6 +168,35 @@ namespace Chapter7
 	}
 
 	template<class T>
+	void Sort<T>::HeapSort(T* a, const int n)
+	{
+		for (int i = n / 2; i >= 1; --i)
+			Adjust(a, i, n);
+
+		for (int i = n - 1; i >= 1; --i)
+		{
+			swap(a[1], a[i + 1]);
+			Adjust(a, 1, i);
+		}
+	}
+
+	template<class T>
+	void Sort<T>::Adjust(T* a, const int root, const int n)
+	{
+		T e = a[root];
+		for(int j = 2*root; j <= n;j *=2)
+		{
+			if (j < n && a[i] < a[j + 1])
+				j++;
+			if (e >= a[j])
+				break;
+			a[j / 2] = a[j];
+		}
+		a[j / 2] = e;
+	}
+
+
+	template<class T>
 	void Sort<T>::MergePass(T* initList, T* resultList, const int n, const int s)
 	{
 		int  i = 1;
