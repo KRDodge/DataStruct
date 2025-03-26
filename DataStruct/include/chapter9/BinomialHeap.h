7db154ea11834967d8a82b18a2ccea81cc130104
KRDodge <20012011ryu@gmail.com>
2025-03-26 18:25:01 +0900

    3/26 이항트리 gpt 도움받음

---
+++ DataStruct/include/chapter9/BinomialHeap.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "chapter5/MinPQ.h"
+#include <iostream>
+
+using namespace Chapter5;
+using namespace std;
+
+namespace Chapter9
+{
+	template <class T>
+	class BinomialNode
+	{
+		friend class BinomialTree<T>;
+	private:
+		T data;
+		BinomialNode<T>* child;
+		BinomialNode<T>* link;
+		int degree;
+	};
+
+	template <class T>
+	class BinomialHeap :public MinPQ<T>
+	{
+	public:
+		BinomialHeap(BinmialNode<T>* init = nullptr) : min(init) {};
+
+		bool IsEmpty() const override;
+		const T& Top() const override;
+		void Pop() override;
+		void Push(const T& p) override;
+
+		const T& Min() const;
+		void Insert(const T&);
+		T& DeleteMin();
+		void Meld(BionomialHeap<T>*);
+
+	private:
+		BinomialNode<T>* min;
+	};
+}
---
+++ DataStruct/src/chapter9/BinomialHeap.cpp
@@ -0,0 +1,117 @@
+#include "chapter9/BinomialHeap.h"
+
+namespace Chapter9
+{
+	template <class T>
+	bool BinomialHeap<T>::IsEmpty() const
+	{
+		return min == nullptr;
+	}
+
+	template<class T>
+	const T& BinomialHeap<T>::Top() const
+	{
+		if (IsEmpty())
+			throw "Tree is empty";
+		return min->data;
+	}
+
+	template<class T>
+	void BinomialHeap<T>::Pop()
+	{
+		if (IsEmpty())
+			throw "Tree is empty";
+
+		DeleteMin();
+	}
+
+	template<class T>
+	void BinomialHeap<T>::Push(const T& p)
+	{
+		Insert(p);
+	}
+
+	template<class T>
+	const T& BinomialHeap<T>::Min() const
+	{
+		if (IsEmpty())
+			throw "Tree is empty";
+		return min->data;
+	}
+
+	template<class T>
+	void BinomialHeap<T>::Insert(const T&)
+	{
+		BinomialNode<T>* node = new BinomialNode<T>{ d, nullptr, nullptr, 0 };
+		BinomialHeap<T> temp(node);
+		Meld(&temp);
+	}
+
+	template<class T>
+	T& BinomialHeap<T>::DeleteMin()
+	{
+		if (IsEmpty()) throw "Heap is empty";
+
+		// Step 1: min 찾아서 제거
+		BinomialNode<T>* prevMin = nullptr;
+		BinomialNode<T>* curr = min;
+		BinomialNode<T>* prev = nullptr;
+		BinomialNode<T>* scan = min;
+
+		BinomialNode<T>* minNode = scan;
+		T& minData = minNode->data;
+
+		BinomialNode<T>* child = minNode->child;
+		BinomialNode<T>* reversed = nullptr;
+		while (child) 
+		{
+			BinomialNode<T>* next = child->link;
+			child->link = reversed;
+			reversed = child;
+			child = next;
+		}
+
+		delete minNode;
+		min = nullptr;
+		BinomialHeap<T> childHeap(reversed);
+		Meld(&childHeap);
+
+		return minData;
+	}
+
+	template<class T>
+	void BinomialHeap<T>::Meld(BionomialHeap<T>* b)
+	{
+		if (min == nullptr) 
+		{
+			min = b->min;
+			b->min = nullptr;
+			return;
+		}
+
+		BinomialNode<T>* h1 = min;
+		BinomialNode<T>* h2 = b->min;
+		BinomialNode<T>* newHead = nullptr;
+		BinomialNode<T>** pos = &newHead;
+
+		while (h1 && h2) 
+		{
+			if (h1->degree <= h2->degree) {
+				*pos = h1;
+				h1 = h1->link;
+			}
+			else {
+				*pos = h2;
+				h2 = h2->link;
+			}
+			pos = &((*pos)->link);
+		}
+		if (h1)
+			*pos = h1;
+		else
+			*pos = h2
+
+		min = newHead;
+		b->min = nullptr;
+	}
+}
